62f754afe165ed3a04de048e728bc2838026763f
GiuliaGuarinoo <128474592+GiuliaGuarinoo@users.noreply.github.com>
2023-04-18 15:03:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,72 +19,70 @@ for (const boxitem of allboxes){
 }
 
 function change_theme(event){
-if (theme){
-  create_randombck();
-  
-} else{
-  create_random_album();
-}
+
   const bckground_rest = document.querySelectorAll('.choice-grid div');
   const unchecked_rest = document.querySelectorAll('.checkbox'); 
+
+    if (theme){
+      create_randombck();
+    } else{
+    create_random_album();
+    }
   
-  for (const i of unchecked_rest){
-      
+    for (const i of unchecked_rest){
       i.src = image_unchecked;
-      } 
+    } 
   
-  for (const bck of bckground_rest) {
-    
+    for (const bck of bckground_rest) {
      bck.classList.remove('chosen');
      bck.classList.remove('no-chosen');
      checkboxes.splice(0,num);
-    
-   } 
+    } 
 }
 
 function change_to_checked(event){
 
-    const img_check = event.currentTarget.querySelector('.checkbox');
-    const img_chosen = event.currentTarget;
-    const bckground = event.currentTarget.parentNode.querySelectorAll('.all')
-    const unchecked = event.currentTarget.parentNode.querySelectorAll('.checkbox'); 
+  const img_check = event.currentTarget.querySelector('.checkbox');
+  const img_chosen = event.currentTarget;
+  const bckground = event.currentTarget.parentNode.querySelectorAll('.all')
+  const unchecked = event.currentTarget.parentNode.querySelectorAll('.checkbox'); 
     
     for (const i of unchecked){
-        
         i.src = image_unchecked;
     } 
     
     for (const bck of bckground) {
-
-       bck.classList.add('no-chosen');
-       bck.classList.remove('chosen');
-
+        bck.classList.add('no-chosen');
+        bck.classList.remove('chosen');
     } 
      
-     img_check.src = image_checked;
-     img_chosen.classList.remove('no-chosen');
-     img_chosen.classList.add('chosen');
+    img_check.src = image_checked;
+    img_chosen.classList.remove('no-chosen');
+    img_chosen.classList.add('chosen');
 
     unchecked_ok(img_chosen);
     checkboxes.push(img_chosen);
 
     if (checkboxes.length===num){
       answer()
-      for (const boxitem of allboxes){
-      boxitem.removeEventListener("click", change_to_checked);
-      }
-      choice_theme.removeEventListener("click", change_theme);
+
+        for (const boxitem of allboxes){
+            boxitem.removeEventListener("click", change_to_checked);
+        }
+            choice_theme.removeEventListener("click", change_theme);
     }
 }
 
 function unchecked_ok(div){
+
       for (let i=0; i<checkboxes.length; i++){
         if (checkboxes[i].dataset.questionId === div.dataset.questionId )
         checkboxes.splice (i,1) 
       }
-    }
+}
 
 function answer(){
+
   let count = 1;
   for (let i=0; i < checkboxes.length; i++){
       for (let j= i+1; j<= checkboxes.length -1; j++){
@@ -95,8 +93,7 @@ function answer(){
         result.title = RESULTS_MAP[checkboxes[i].dataset.choiceId].title
         result.text = RESULTS_MAP[checkboxes[i].dataset.choiceId].contents
         break;
-        
-    }
+      }
       
     else{
       result.title = RESULTS_MAP[checkboxes[0].dataset.choiceId].title
@@ -132,18 +129,18 @@ function restart(){
   for (const i of unchecked_rest){
       
       i.src = image_unchecked;
-      } 
+  } 
   
   for (const bck of bckground_rest) {
     
      bck.classList.remove('chosen');
      bck.classList.remove('no-chosen');
      checkboxes.splice(0,num);
-    
-   } 
+  } 
+
   for (const boxitem of allboxes){
      boxitem.addEventListener("click", change_to_checked);
-    }
+  }
   
     result_container.querySelector('#title').innerHTML = '';
     result_container.querySelector('#description').innerHTML = '';
@@ -154,7 +151,8 @@ function restart(){
  }
 
 function create_randombck(){
-  choice_theme.removeEventListener("click", choice_theme);
+
+  choice_theme.removeEventListener("click", change_theme);
   choice_theme.innerHTML= '';
   const value = document.createElement('h3');
   value.textContent = 'Album';
@@ -162,18 +160,14 @@ function create_randombck(){
   theme= false;
   ans = num_ans * num;
   rest_url =" https://x-colors.yurace.pro/api/random?number=" + ans; 
-
   fetch(rest_url).then(onSuccess).then(onJson);
-  choice_theme.addEventListener("click", choice_theme);
- 
- }
+}
 
 function onText(colore){
    console.log(colore);
- }
+}
 
-function onSuccess(response){
- 
+function onSuccess(response){ 
   return response.json();
 }
 
@@ -185,10 +179,11 @@ function onJson(json){
     bckdiv[i].style.backgroundImage = 'none';
     bckdiv[i].style.backgroundColor = json[i].hex;
   }
+  choice_theme.addEventListener("click", change_theme);
 }
 
 function create_random_album(){
-  choice_theme.removeEventListener("click", choice_theme);
+  choice_theme.removeEventListener("click", change_theme);
   choice_theme.innerHTML= '';
   const value = document.createElement('h3');
   value.textContent = 'Colori'
@@ -206,11 +201,9 @@ function create_random_album(){
       'Authorization': 'Basic ' + btoa(client_id + ":" + secret_id)
     }
   }).then(tokenResponse).then(tokenJson);
-  choice_theme.addEventListener("click", choice_theme);
 }
 
 function tokenResponse(token_Response){
-
   return token_Response.json();
 }
 
@@ -226,7 +219,6 @@ function tokenJson(token_json){
 }
 
 function responseAlbum(response_Album){
-  
   return response_Album.json();
 }
 
@@ -238,6 +230,7 @@ function jsonAlbum(json_Album){
     const img_url = json_Album.albums.items[i].images[1].url;
     bckdiv[i].style.backgroundImage = "url('" + img_url + "')";
   }
+  choice_theme.addEventListener("click", change_theme);
 }
 
 
